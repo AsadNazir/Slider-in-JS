f9cc0c4317446e05bb39aafbe90c4d9df0eae7e1
AsadNazir <79693635+AsadNazir@users.noreply.github.com>
2021-10-07 16:21:28 +0500

    Completed  extremely simple slider in JS
    Added the dots functionality and fixed some of the bugs

--- slider.js
+++ slider.js
@@ -1,9 +1,34 @@
 let count = 1;
+let slides = 0;
 let img = document.querySelectorAll(`.img`);
 let bright = document.querySelector(`.right`)
 let bleft = document.querySelector(`.left`);
-let no = document.querySelector(`.span`);
-//let count = 1;
+let ul = document.querySelector(`.dot_cont`);
+
+//---------------------------------------
+img.forEach(function(ele, index) {
+    slides++;
+})
+for (let i = 0; i < slides; i++) {
+
+    ul.innerHTML += `<li class="dots"></li>`;
+
+}
+//---------------------------------------
+let dots = document.querySelectorAll(`.dots`);
+
+
+
+//Dots Functionality over here
+dots.forEach(function(ele, index) {
+
+        ele.addEventListener("click", function() {
+
+            count = index + 1;
+            showSlides();
+        })
+    })
+    //Showslides function
 function showSlides() {
     if (count > img.length) {
         count = 1;
@@ -11,22 +36,28 @@ function showSlides() {
     if (count < 1) {
         count = img.length;
     }
-    img.forEach(function(ele, index) {
-        ele.style.opacity = `0`;
-        console.log(index);
-    });
+
+    for (let i = 0; i < slides; i++) {
+        img[i].style.opacity = `0`;
+        dots[i].style.backgroundColor = `transparent`;
+        dots[i].style.transform = `scale(0.9)`;
+
+    }
     img[count - 1].style.opacity = `1`;
-    console.log(count);
-    no.innerHTML = `${count}/3`;
+    dots[count - 1].style.backgroundColor = `white`;
+    dots[count - 1].style.transform = `scale(1.2)`;
+
+
 }
 showSlides();
+//Right Button
 bright.addEventListener('click', function() {
-    count++;
-    showSlides()
-})
-
+        count++;
+        showSlides()
+    })
+    //Left Button
 bleft.addEventListener('click', function() {
-    //checker()
+
     count--;
     showSlides();
 })
